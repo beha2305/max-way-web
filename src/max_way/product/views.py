c6423313a41ae6ec091a3d46c537fde90fcea71d
behruz2305 <behruzraimqulov3@gmail.com>
2024-03-10 02:24:43 +0500

    ok

--- src/max_way/product/views.py
+++ src/max_way/product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Category
+from . import services
 
-# Create your views here.
+def index(request):
+    category_id = request.GET.get("category")
+    products = services.get_products(category_id=category_id)
+    categories = Category.objects.all()
+
+    print(category_id)
+    ctx = {
+        "products": products,
+        "categories": categories
+    }
+
+    return render(request, "index.html", ctx)
--- src/max_way/urls.py
+++ src/max_way/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('max_way.user.urls')),
-]
+    path('', include('max_way.product.urls')),
+    path('category/', include('max_way.product.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
